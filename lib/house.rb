60aaee933717050d3caa6bcd2faf60a6fcc60eff
Adrianna Guevarra <guevarra@oregonstate.edu>
2021-04-15 10:44:47 -0700

    introduce lyrics of line 6

--- lib/house.rb
+++ lib/house.rb
@@ -13,6 +13,8 @@ class House
             number = "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 5
             number = "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+        when 6
+            number = "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         else
             number = "This is the house that Jack built.\n"
         
