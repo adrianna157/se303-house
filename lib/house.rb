0f39d9240662a80271c5a5a64395618eb8f78fe8
Adrianna Guevarra <guevarra@oregonstate.edu>
2021-04-15 10:39:53 -0700

    add switch statement and introduce lyrics of line 2

--- lib/house.rb
+++ lib/house.rb
@@ -1,6 +1,21 @@
 class House
     def line(number)
-        number = "This is the house that Jack built.\n"
+        number = randomizer(number)
         return number
     end
+    def randomizer(number)
+        case number
+        when 2
+            number = "This is the malt that lay in the house that Jack built.\n"
+        when 3
+            
+        else
+            number = "This is the house that Jack built.\n"
+        
+            return number
+        end
+            
+    end
+       
+
 end
