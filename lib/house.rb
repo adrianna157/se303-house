e0a3f0d8846ad7a08248f52f81604c42d4cd9118
Adrianna Guevarra <guevarra@oregonstate.edu>
2021-05-18 20:11:37 -0700

    made line 11 and 12 the same

--- lib/house.rb
+++ lib/house.rb
@@ -28,7 +28,8 @@ class House
         'the dog that worried',
         'the cat that killed',
         'the rat that ate',
-        'the malt that lay in'
+        'the malt that lay in',
+        'the house that Jack built',
     ]
     end
 
@@ -55,9 +56,9 @@ class House
         when 10
            "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 11
-            "#{starting_clause(number)} #{pieces.last(number).join(' ')} the house that Jack built.\n"
+            "#{starting_clause(number)} #{pieces.last(number).join(' ')}.\n"
         when 12
-            "#{starting_clause(number)} #{pieces.last(number).join(' ')} the house that Jack built.\n"
+            "#{starting_clause(number)} #{pieces.last(number).join(' ')}.\n"
         end
             
     end
