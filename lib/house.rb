d03a75fff743af0a55d84f998c8b21ccfc66370f
Adrianna Guevarra <guevarra@oregonstate.edu>
2021-05-18 18:59:29 -0700

    apply flocking rule: find similarity between line 11 and 12 and find the difference then try to make them the same

--- lib/house.rb
+++ lib/house.rb
@@ -3,9 +3,19 @@ class House
     def recite
        (1..12).collect{|i| line(i)}.join("\n")
     end
-    
+
+    def phrase (number)
+        if number == 11
+            "farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in" + "house that Jack built"
+        else
+            "house that Jack built"
+        end
+    end
+
     def line(number)
         case number
+        when 1
+            "This is the house that Jack built.\n"
         when 2
             "This is the malt that lay in the house that Jack built.\n"
         when 3
@@ -28,8 +38,6 @@ class House
             "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 12
             "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-        when 1
-            "This is the house that Jack built.\n"
         end
             
     end
