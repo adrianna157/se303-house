e97ea5e9aae8606b5534c85239b83d81dc551c97
Adrianna Guevarra <guevarra@oregonstate.edu>
2021-05-18 20:14:38 -0700

    made changes the same so that 1 - 12 look the same

--- lib/house.rb
+++ lib/house.rb
@@ -36,23 +36,23 @@ class House
     def line(number)
         case number
         when 1
-            "This is the house that Jack built.\n"
+            "#{starting_clause(number)} #{pieces.last(number).join(' ')}.\n"
         when 2
-            "This is the malt that lay in the house that Jack built.\n"
+            "#{starting_clause(number)} #{pieces.last(number).join(' ')}.\n"
         when 3
-            "This is the rat that ate the malt that lay in the house that Jack built.\n"
+            "#{starting_clause(number)} #{pieces.last(number).join(' ')}.\n"
         when 4
-            "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            "#{starting_clause(number)} #{pieces.last(number).join(' ')}.\n"
         when 5
-            "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            "#{starting_clause(number)} #{pieces.last(number).join(' ')}.\n"
         when 6
-            "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            "#{starting_clause(number)} #{pieces.last(number).join(' ')}.\n"
         when 7
-            "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            "#{starting_clause(number)} #{pieces.last(number).join(' ')}.\n"
         when 8
-           "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+           "#{starting_clause(number)} #{pieces.last(number).join(' ')}.\n"
         when 9
-            "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            "#{starting_clause(number)} #{pieces.last(number).join(' ')}.\n"
         when 10
            "#{starting_clause(number)} #{pieces.last(number).join(' ')}.\n"
         when 11
