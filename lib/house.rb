d703e1b3b94123d6c60a03c01e870ae625710106
Adrianna Guevarra <guevarra@oregonstate.edu>
2021-05-18 20:08:07 -0700

    chose to refactor phrase method

--- lib/house.rb
+++ lib/house.rb
@@ -4,12 +4,8 @@ class House
        (1..12).collect{|i| line(i)}.join("\n")
     end
 
-    def phrase (number)
-        if number 
-            "the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in" 
-        else
-            "the house that Jack built"
-        end
+    def end_phrase
+        return "the house that Jack built"
     end
 
     def starting_clause(number)
@@ -24,6 +20,15 @@ class House
     [
         'the horse and the hound and the horn that belonged to',
         'the farmer sowing his corn that kept',
+        'the rooster that crowed in the morn that woke',
+        'the priest all shaven and shorn that married',
+        'the man all tattered and torn that kissed',
+        'the maiden all forlorn that milked',
+        'the cow with the crumpled horn that tossed',
+        'the dog that worried',
+        'the cat that killed',
+        'the rat that ate',
+        'the malt that lay in'
     ]
     end
 
@@ -50,9 +55,9 @@ class House
         when 10
            "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 11
-            "#{starting_clause(number)} #{phrase(number)} the house that Jack built.\n"
+            "#{starting_clause(number)} #{pieces.last(number).join(' ')} the house that Jack built.\n"
         when 12
-            "#{starting_clause(number)} #{pieces.last(number).join(' ')} #{phrase(number)} the house that Jack built.\n"
+            "#{starting_clause(number)} #{pieces.last(number).join(' ')} the house that Jack built.\n"
         end
             
     end
