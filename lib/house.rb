213ed3b62d80a77200a13573bdc11de983b1c860
Adrianna Guevarra <guevarra@oregonstate.edu>
2021-05-18 20:17:31 -0700

    delete duplicate code and end_phrase method

--- lib/house.rb
+++ lib/house.rb
@@ -4,9 +4,6 @@ class House
        (1..12).collect{|i| line(i)}.join("\n")
     end
 
-    def end_phrase
-        return "the house that Jack built"
-    end
 
     def starting_clause(number)
         if number
@@ -35,27 +32,7 @@ class House
 
     def line(number)
         case number
-        when 1
-            "#{starting_clause(number)} #{pieces.last(number).join(' ')}.\n"
-        when 2
-            "#{starting_clause(number)} #{pieces.last(number).join(' ')}.\n"
-        when 3
-            "#{starting_clause(number)} #{pieces.last(number).join(' ')}.\n"
-        when 4
-            "#{starting_clause(number)} #{pieces.last(number).join(' ')}.\n"
-        when 5
-            "#{starting_clause(number)} #{pieces.last(number).join(' ')}.\n"
-        when 6
-            "#{starting_clause(number)} #{pieces.last(number).join(' ')}.\n"
-        when 7
-            "#{starting_clause(number)} #{pieces.last(number).join(' ')}.\n"
-        when 8
-           "#{starting_clause(number)} #{pieces.last(number).join(' ')}.\n"
-        when 9
-            "#{starting_clause(number)} #{pieces.last(number).join(' ')}.\n"
-        when 10
-           "#{starting_clause(number)} #{pieces.last(number).join(' ')}.\n"
-        when 11
+        when number
             "#{starting_clause(number)} #{pieces.last(number).join(' ')}.\n"
         end
             
