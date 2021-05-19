19523ec21fa080f6526ce5d31faa112dbe122431
Adrianna Guevarra <guevarra@oregonstate.edu>
2021-05-18 19:20:31 -0700

    make line 11 and 12 look alike

--- lib/house.rb
+++ lib/house.rb
@@ -12,6 +12,14 @@ class House
         end
     end
 
+    def starting_clause(number)
+        if number
+            "This is the"
+        else
+            return
+        end
+    end
+
     def line(number)
         case number
         when 1
@@ -35,9 +43,9 @@ class House
         when 10
            "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 11
-            "This is the #{phrase(number)} the house that Jack built.\n"
+            "#{starting_clause(number)} #{phrase(number)} the house that Jack built.\n"
         when 12
-            "This is the horse and the hound and the horn that belonged to the #{phrase(number)} the house that Jack built.\n"
+            "#{starting_clause(number)} horse and the hound and the horn that belonged to the #{phrase(number)} the house that Jack built.\n"
         end
             
     end
